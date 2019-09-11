bd31a09e08b51ecedb22efda3a1ff9aa4830e651
Monbrey <rsm999@uowmail.edu.au>
2019-09-11 12:04:20 +1000

    Extend the GuildMember object to represent trainers

---
+++ src/lib/structures/GuildTrainer.ts
@@ -0,0 +1,27 @@
+import { Guild, Structures } from "discord.js";
+import { GuildMember } from "discord.js";
+import KauriClient from "../../client/KauriClient";
+import { ITrainer, Trainer } from "../../models/trainer";
+import MongooseProvider from "../../providers/MongooseProvider";
+
+const Trainers = new MongooseProvider(Trainer, "_id");
+
+// tslint:disable-next-line: no-shadowed-variable
+Structures.extend("GuildMember", GuildMember => {
+    // tslint:disable-next-line: no-shadowed-variable
+    class GuildTrainer extends GuildMember {
+        public trainer: ITrainer = new Trainer();
+
+        constructor(client: KauriClient, data: any, guild: Guild) {
+            super(client, data, guild);
+
+            this.resolveTrainer();
+        }
+
+        private async resolveTrainer() {
+            this.trainer = await Trainers.fetch(this.id) || await Trainers.add(new Trainer({ _id: this.id }));
+        }
+    }
+
+    return GuildTrainer;
+});
