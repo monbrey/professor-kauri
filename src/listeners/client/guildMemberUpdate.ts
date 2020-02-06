0579a7cda73870bcaef6aa4d4debc45a86f606d8
Monbrey <rsm999@uowmail.edu.au>
2020-02-06 14:12:00 +1100

    Remove outdated nickname protection

--- src/listeners/client/guildMemberUpdate.ts
+++ src/listeners/client/guildMemberUpdate.ts
@@ -10,12 +10,6 @@ export default class GuildMemberUpdateListener extends Listener {
     }
 
     public async exec(oldMember: GuildMember, newMember: GuildMember) {
-        if (oldMember.id === oldMember.guild.me!.id && newMember.nickname != null) {
-            try {
-                await newMember.setNickname("");
-                if (newMember.guild.systemChannel) { newMember.guild.systemChannel.embed("Please don't weebify my name."); }
-            } catch (e) { this.client.logger.parseError(e); }
-            return this.client.logger.guildMemberUpdate(oldMember, newMember);
-        }
+        return this.client.logger.guildMemberUpdate(oldMember, newMember);
     }
 }
