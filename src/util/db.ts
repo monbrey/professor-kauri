51325ef8a57a8cfeec47f0ccb4d19903e9876643
monbrey <monbrey@gmail.com>
2020-09-29 10:06:26 +1000

    fix(db): catch connection errors

--- src/util/db.ts
+++ src/util/db.ts
@@ -3,7 +3,15 @@ import { MongooseOptions } from "./constants";
 
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(`${process.env.KAURIDB_URI!}/monbrey-urpg-v2`, MongooseOptions);
+mongoose.connect(`${process.env.KAURIDB_URI!}/monbrey-urpg-v2`, MongooseOptions)
+    .catch(e => {
+        console.error(e);
+        process.exit()
+    });
 
 export const db = mongoose.connection;
-export const instanceDB = mongoose.createConnection(`${process.env.KAURIDB_URI!}/${process.env.NODE_ENV || "development"}`, MongooseOptions);
+export const instanceDB = mongoose.createConnection(`${process.env.KAURIDB_URI!}/${process.env.NODE_ENV || "development"}`, MongooseOptions)
+    .catch(e => {
+        console.error(e);
+        process.exit()
+    });
