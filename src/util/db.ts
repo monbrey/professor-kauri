90ffb780ec68f39e785cf5805acd062bc2b8dc52
monbrey <monbrey@gmail.com>
2020-10-02 09:10:32 +1000

    revert(db): dont export promises

--- src/util/db.ts
+++ src/util/db.ts
@@ -4,14 +4,6 @@ import { MongooseOptions } from "./constants";
 mongoose.set("useCreateIndex", true);
 
 mongoose.connect(`${process.env.KAURIDB_URI!}/monbrey-urpg-v2`, MongooseOptions)
-    .catch(e => {
-        console.error(e);
-        process.exit();
-    });
 
 export const db = mongoose.connection;
 export const instanceDB = mongoose.createConnection(`${process.env.KAURIDB_URI!}/${process.env.NODE_ENV || "development"}`, MongooseOptions)
-    .catch(e => {
-        console.error(e);
-        process.exit();
-    });
