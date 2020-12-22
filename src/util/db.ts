4b4a0b7aefe9efe83214d4c6a6d4e9f702fa35ac
monbrey <monbrey@gmail.com>
2020-12-22 19:56:42 +1100

    chore: security

--- src/util/db.ts
+++ src/util/db.ts
@@ -3,7 +3,7 @@ import { MongooseOptions } from "./constants";
 
 mongoose.set("useCreateIndex", true);
 
-mongoose.connect(`${process.env.KAURIDB_URI!}/monbrey-urpg-v2`, MongooseOptions)
+mongoose.connect(`${process.env.KAURIDB_URI!}/monbrey-urpg-v2`, MongooseOptions);
 
 export const db = mongoose.connection;
-export const instanceDB = mongoose.createConnection(`${process.env.KAURIDB_URI!}/${process.env.NODE_ENV || "development"}`, MongooseOptions)
+export const instanceDB = mongoose.createConnection(`${process.env.KAURIDB_URI!}/${process.env.NODE_ENV || "development"}`, MongooseOptions);
