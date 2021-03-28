30a1b75c0e7eaf1c454af5d9246cc4e865314a78
monbrey <monbrey@gmail.com>
2021-03-29 08:19:58 +1100

    fix: Slash Commands in DMs

--- src/listeners/websocket/interactionCreate.ts
+++ src/listeners/websocket/interactionCreate.ts
@@ -1,4 +1,5 @@
 import { Listener } from "discord-akairo";
+import { DMChannel } from "discord.js";
 import KauriClient from "../../client/KauriClient";
 import { KauriCommand } from "../../lib/commands/KauriCommand";
 import { KauriMessage } from "../../lib/structures/KauriMessage";
@@ -13,18 +14,20 @@ export default class extends Listener {
   }
 
   public async exec(interaction: any) {
+    console.log(interaction);
     const name = interaction.data.name;
     const command = this.client.commandHandler.findCommand(name) as KauriCommand;
 
     if (command?.interact) {
-      const channel = this.client.channels.cache.get(interaction.channel_id);
-      if (!channel?.isText()) return;
+      const channel = this.client.channels.cache.get(interaction.channel_id) || new DMChannel(this.client, { id: interaction.channel_id });
+      if(!channel?.isText()) return;
 
-      const message = new KauriMessage(this.client as KauriClient, { ...interaction, author: interaction.member.user }, channel);
+      const message = new KauriMessage(this.client as KauriClient, { ...interaction, author: interaction.member?.user || interaction.user }, channel);
 
       const args = argMapper(interaction.data.options ?? []);
 
       const inhibited = await command.handler.runPostTypeInhibitors(message, command);
+      console.log(inhibited);
 
       if (!inhibited) {
         command.interact(message, args);
