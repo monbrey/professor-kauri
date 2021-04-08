b64af7f7c0a3ce01a79176a4f4735109779dde4f
Monbrey <rsm999@uowmail.edu.au>
2021-04-08 16:42:58 +1000

    feat: weather slash command migration

---
+++ src/interactions/info/weather.ts
@@ -0,0 +1,52 @@
+import { CommandInteraction, MessageEmbed } from "discord.js";
+import { InteractionCommand } from "../../lib/commands/InteractionCommand";
+import { IWeather, Weather } from "../../models/mongo/weather";
+
+export default class extends InteractionCommand {
+  constructor() {
+    super("weather", {
+      name: "weather",
+      description: "Retrieve weather effect information",
+      options: [{
+        name: "weather",
+        description: "Weather effect to look-up",
+        type: "INTEGER",
+        choices: [
+          { "value": 1, "name": "Rain" },
+          { "value": 2, "name": "Harsh Sunlight" },
+          { "value": 3, "name": "Sandstorm" },
+          { "value": 4, "name": "Hail" },
+          { "value": 5, "name": "Fog" },
+          { "value": 6, "name": "Extremely Harsh Sunlight" },
+          { "value": 7, "name": "Heavy Rain" },
+          { "value": 8, "name": "Mysterious Air Current" }
+        ]
+      }],
+      guild: true
+    });
+  }
+
+  public async exec(interaction: CommandInteraction, args: Map<string, any>) {
+    const weather = args.get("weather");
+
+    if (!weather) {
+      const weathers: IWeather[] = await Weather.find({});
+      const list = weathers.map(w => `${this.client.emojis.cache.get(w.emoji) ?? w.emoji} ${w.weatherName}`);
+
+      const embed = new MessageEmbed()
+        .setTitle("Weather Conditions")
+        .setColor(0xFFFFFF)
+        .setDescription(`${list.join("\n")}\n\nFor more information on any weather condition, use \`/weather [name]\``);
+
+      return interaction.reply(embed);
+    }
+
+    try {
+      const thisWeather = await Weather.findById(weather);
+      if (!thisWeather) return interaction.reply("No results found", { ephemeral: true });
+      return interaction.reply(thisWeather.info(this.client));
+    } catch (e) {
+      this.client.logger.parseError(e);
+    }
+  }
+}
