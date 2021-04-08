0b0f58f7ec9abf95cad939fc0dc61727b5ced2ec
monbrey <monbrey@gmail.com>
2021-04-08 22:29:36 +1000

    fix: remove dppt, make oras global

--- src/interactions/info/dppt.ts
+++ src/interactions/info/dppt.ts
@@ -1,42 +1,41 @@
-import { Collection, CommandInteraction, MessageEmbed } from "discord.js";
-import { InteractionCommand } from "../../lib/commands/InteractionCommand";
-import { CommandExecutionError } from "../../lib/misc/CommandExecutionError";
+// import { Collection, CommandInteraction, MessageEmbed } from "discord.js";
+// import { InteractionCommand } from "../../lib/commands/InteractionCommand";
+// import { CommandExecutionError } from "../../lib/misc/CommandExecutionError";
 
-export default class extends InteractionCommand {
-  constructor() {
-    super("dppt", {
-      name: "dppt",
-      description: "Get DPPt Contest data for a move",
-      options: [{
-        name: "move",
-        description: "Name of the move to search for",
-        type: "STRING",
-        required: true
-      }],
-      guild: true
-    });
-  }
+// export default class extends InteractionCommand {
+//   constructor() {
+//     super("dppt", {
+//       name: "dppt",
+//       description: "Get DPPt Contest data for a move",
+//       options: [{
+//         name: "move",
+//         description: "Name of the move to search for",
+//         type: "STRING",
+//         required: true
+//       }],
+//     });
+//   }
 
 
-  public async exec(interaction: CommandInteraction) {
-    const query = interaction.options.find(o => o.name === "move")?.value as string;
-    if (!query) throw new CommandExecutionError("Command parameter 'move' not found");
+//   public async exec(interaction: CommandInteraction) {
+//     const query = interaction.options.find(o => o.name === "move")?.value as string;
+//     if (!query) throw new CommandExecutionError("Command parameter 'move' not found");
 
-    const { value } = await this.client.urpg.attack.fetchClosest(query);
-    if (!value) throw new CommandExecutionError(`No move found matching \`${query}\``);
+//     const { value } = await this.client.urpg.attack.fetchClosest(query);
+//     if (!value) throw new CommandExecutionError(`No move found matching \`${query}\``);
 
-    this.client.logger.info({
-      key: interaction.commandName,
-      query,
-      result: value.name
-    });
+//     this.client.logger.info({
+//       key: interaction.commandName,
+//       query,
+//       result: value.name
+//     });
 
-    const { dppContestAttribute: attribute, dppContestMoveType: { score, description } } = value;
+//     const { dppContestAttribute: attribute, dppContestMoveType: { score, description } } = value;
 
-    const embed = new MessageEmbed()
-      .setTitle(value.name)
-      .addField(`| Attribute: ${attribute} | Score: ${score > 0 ? `+${score}` : `${score}`} |`, description);
+//     const embed = new MessageEmbed()
+//       .setTitle(value.name)
+//       .addField(`| Attribute: ${attribute} | Score: ${score > 0 ? `+${score}` : `${score}`} |`, description);
 
-    return interaction.reply(embed);
-  }
-}
+//     return interaction.reply(embed);
+//   }
+// }
--- src/interactions/info/oras.ts
+++ src/interactions/info/oras.ts
@@ -13,7 +13,6 @@ export default class extends InteractionCommand {
         type: "STRING",
         required: true
       }],
-      guild: true
     });
   }
 
